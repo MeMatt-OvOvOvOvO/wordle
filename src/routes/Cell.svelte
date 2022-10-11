5f9a4589885f5dcff190621e26f89e9fb1404215
Mateusz Janicki <85708973+MeMatt-OvOvOvOvO@users.noreply.github.com>
2022-10-11 11:52:18 +0200

    Update Cell.svelte

--- src/routes/Cell.svelte
+++ src/routes/Cell.svelte
@@ -13,3 +13,10 @@
 <div class="w-16 h-16 bg-gray-800 cursor-default text-white m-1 flex items-center justify-center rounded text-4xl">
     { letter }
 </div>
+
+<style>
+    :global(body.light-mode) div {
+		background-color: gray;
+		color: white;
+	}
+</style>
